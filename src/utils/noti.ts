1c86db1f06590f0e01ae41a433d4584a976ae6ac
giangit112000 <giangnt.it0101@gmail.com>
2022-10-04 15:31:21 +0700

    init project

--- src/utils/noti.ts
+++
@@ -1,8 +0,0 @@
-import { notification } from 'antd';
-
-export const openNotificationWithIcon = (noti) => {
-  notification[noti.type]({
-    message: noti.message,
-    description: noti.description,
-  });
-};
